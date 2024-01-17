9329c6cea5b5ad749934e08539158bd5769e8000
ValerioCarbone <valeriocarbone10@gmail.com>
2024-01-17 10:11:27 +0100

    ADD - wrapper bg color gradient

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,18 @@ export default {
 </script>
 
 <template>
-  <AppHeader></AppHeader>
-  <router-view></router-view>
+  <div class="wrapper">
+    <AppHeader></AppHeader>
+    <router-view></router-view>
+  </div>
 </template>
 
 <style lang="scss">
 @use "./style/general.scss";
+
+.wrapper {
+  background-color: #8EC5FC;
+  background-image: linear-gradient(180deg, #8EC5FC 0%, #E0C3FC 99%);
+  height: 100vh;
+}
 </style>
